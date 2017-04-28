670352e4316ee4d0bdf1278946672c683bbab98b
Yuriy Kuzin <yuriy.kuzin@gmail.com>
2017-04-28 18:26:10 +0300

    remove import HttpModule

--- src/app/ng2wig/ng2wig.module.ts
+++ src/app/ng2wig/ng2wig.module.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { Ng2WigComponent } from './ng2wig.component';
 import { Ng2WigToolbarService } from './ng2wig-toolbar.service';
@@ -13,7 +12,6 @@ import { Ng2WigToolbarService } from './ng2wig-toolbar.service';
   imports: [
     CommonModule,
     FormsModule,
-    HttpModule
   ],
   providers: [Ng2WigToolbarService],
   bootstrap: [Ng2WigComponent],
