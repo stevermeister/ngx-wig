a2d2c128cefeec1406e4a7aa15eadf31b3c6b794
Yuriy Kuzin <yuriy.kuzin@gmail.com>
2017-05-02 13:10:06 +0300

    add .npmignore

--- dist/ng2wig-toolbar.service.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Ng2WigToolbarService {
-    private _buttonLibrary;
-    private _defaultButtonsList;
-    constructor();
-    setButtons(buttons: string[]): void;
-    addStandardButton(name: string, title: string, command: string, styleClass: string): void;
-    addCustomButton(name: string, pluginName: string): void;
-    getToolbarButtons(buttonsList?: string): {}[];
-}
