288413e82b243d110d9f2c5e8b2ac970034125e8
Stepan Suvorov <stevermeister@gmail.com>
2018-01-12 13:36:39 +0100

    swtich tests to  PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -70,7 +70,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeHeadless'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
