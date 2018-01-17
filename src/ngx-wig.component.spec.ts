53c8adfcb29f81a6f9fec22d2404af2464689c01
Aristeidis Bampakos <bampakoa@gmail.com>
2018-01-17 22:48:37 +0200

    Add early test for main component

---
+++ src/ngx-wig.component.spec.ts
@@ -0,0 +1,52 @@
+import { Component, DebugElement } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+
+import { NgxWigComponent } from './ngx-wig.component';
+import { NgxWigToolbarService } from './ngx-wig-toolbar.service';
+
+let fixture: ComponentFixture<NgxWigComponent>;
+let toolbarService: NgxWigToolbarService;
+let page: Page;
+
+describe('NgxWigComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule],
+      declarations: [NgxWigComponent],
+      providers: [NgxWigToolbarService]
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(NgxWigComponent);
+      page = new Page();
+      fixture.detectChanges();
+      page.addPageElements();
+    });
+  }));
+
+  it('should create the component', () => {
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should create a standard button', () => {
+    expect(page.unorderedListBtn.classes['nw-button-mdi']).toBeDefined();
+    expect(page.unorderedListBtn.nativeElement.getAttribute('title')).toBe('Unordered List');
+    expect(page.unorderedListBtn.nativeElement.textContent).toContain('Unordered List');
+  });
+
+  it('should configure the EditHTML button', () => {
+    expect(page.editHTMLBtn.classes['nw-button-mdi']).toBeDefined();
+  });
+});
+
+class Page {
+  unorderedListBtn: DebugElement;
+  editHTMLBtn: DebugElement;
+
+  addPageElements() {
+    const buttons = fixture.debugElement.queryAll(By.css('button'));
+    this.unorderedListBtn = buttons[0];
+    this.editHTMLBtn = buttons[5];
+  }
+}
